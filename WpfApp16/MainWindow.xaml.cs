646c0993f5649f61671db034d8c16123f77d00b4
maciej022 <maciejpieper56@gmail.com>
2025-03-11 13:58:13 +0100

    zmiany

--- WpfApp16/MainWindow.xaml.cs
+++ WpfApp16/MainWindow.xaml.cs
@@ -27,26 +27,87 @@ namespace WpfApp16
         string[] haslo = new string[4];
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"Dane pracownika: {imie.Text}, {nazwisko.Text}, {stanowisko.SelectedItem as ComboBoxItem}, Hasło: {haslo}");
+            MessageBox.Show($"Dane pracownika: {imie.Text}, {nazwisko.Text}, {stanowisko.SelectedItem as ComboBoxItem}, Hasło: {haslo.ToString()}");
         }
-
         private void generuj_Click(object sender, RoutedEventArgs e)
         {
-            int licznik = 1;
+            int licznik = 0;
             Random random = new Random();
             if (cyfry.IsChecked == true && maleiduze.IsChecked == true && specjalne.IsChecked == true)
             {
-                while(licznik <= 4 )
+                while(licznik < 4 )
                 {
-                    haslo[licznik] = duze[random.Next(duze.Length -1)].ToString();
-                    licznik++;//
+                    haslo[licznik] = duze[random.Next(duze.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = male[random.Next(male.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = cyfryy[random.Next(cyfryy.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = specjalnee[random.Next(specjalnee.Length)].ToString();
+                    licznik++;
+                }
+            } 
+            else if (cyfry.IsChecked == true && maleiduze.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = duze[random.Next(duze.Length)].ToString();
+                    licznik++;
                     haslo[licznik] = male[random.Next(male.Length)].ToString();
                     licznik++;
                     haslo[licznik] = cyfryy[random.Next(cyfryy.Length)].ToString();
                     licznik++;
                 }
-            } 
             }
+            else if (cyfry.IsChecked == true && specjalne.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = specjalnee[random.Next(specjalnee.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = cyfryy[random.Next(cyfryy.Length)].ToString();
+                    licznik++;
+                }
+            }
+            else if (maleiduze.IsChecked == true && specjalne.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = specjalnee[random.Next(specjalnee.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = male[random.Next(male.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = duze[random.Next(duze.Length)].ToString();
+                    licznik++;
+                }
+            }
+            else if (maleiduze.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = male[random.Next(male.Length)].ToString();
+                    licznik++;
+                    haslo[licznik] = duze[random.Next(duze.Length)].ToString();
+                    licznik++;
+                }
+            }
+            else if (specjalne.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = specjalnee[random.Next(specjalnee.Length)].ToString();
+                    licznik++;
+                }
+            }
+            else if (cyfry.IsChecked == true)
+            {
+                while (licznik < 4)
+                {
+                    haslo[licznik] = cyfryy[random.Next(cyfryy.Length)].ToString();
+                    licznik++;
+                }
+            }
+        }
 
         }
     }
